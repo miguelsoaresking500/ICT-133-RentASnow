712fdbf4a87133820db0cff04fb646f013174591
Miguel Soares <miguel.soares@cpnv.ch>
2020-06-08 10:34:03 +0200

    new exercice

---
+++ Prepa/.constant.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Created by PhpStorm.
+ * User: miguel.soares
+ * Date: 07.02.2020
+ * Time: 08:09
+ */
+$user="UserDev";
+$pass="Mikmik500@";
+$dbhost='localhost';
+$dbname='app_pfinfo';
+?>
