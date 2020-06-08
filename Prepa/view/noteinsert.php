5700cdc79d69680e6a43299da27cff355097c906
Miguel Soares <47849626+miguelsoaresking500@users.noreply.github.com>
2020-06-08 16:24:43 +0200

    add fin

--- Prepa/view/noteinsert.php
+++ Prepa/view/noteinsert.php
@@ -6,32 +6,47 @@
  * Time: 10:56
  */
 
+
+if (isset($_POST["store"])) {
+    extract($_POST); // idEval, idStudent, $gradeValue
+    $newGrade = insert("Insert into grade (gradeValue,fkStudent,fkEval) values (:grade,:student,:eval)",
+        [
+            "grade" => $gradeValue,
+            "student" => $idStudent,
+            "eval" => $idEval
+        ]);
+}
 ?>
-    <form action="index.php?action=tryinsert" method="post">
-<FORM>
+
+
+    <form method="post">
+        <BR>
+        <BR>
     <SELECT name="Module" size="1">
         <?php foreach ($module as $modules){?>
         <option><?php echo $modules["moduleShortName"];?></option>
         <?php } ?>
     </SELECT>
-</FORM>
-    <FORM>
-    <SELECT name="Student" size="3">
+        <BR>
+        <BR>
+    <SELECT name="student" size="3">
         <?php foreach ($student as $students){?>
-            <option><?php echo $students["Fullname"];?></option>
+            <option value="<?= $student['idPerson'] ?>"><?php echo $students["Fullname"];?></option>
         <?php } ?>
     </SELECT>
-    </FORM>
-    <FORM>
-        <SELECT name="Eval" size="1">
+        <BR>
+        <BR>
+        <SELECT name="eval" size="1">
             <?php foreach ($eval as $evals){?>
                 <option value="<?php echo $evals["idEvaluation"];?>"><?=$evals["testDescription"]?></option>
             <?php } ?>
         </SELECT>
-    </FORM>
-    <input type="text" id="note" name="note" size="1"  >
+<BR>
+        <BR>
+    <input type="text" name="grade" id="grade" name="note" size="1"  >
     <input type="submit" value="OK">
     </form>
-<?php
 
+<?php
+var_dump($_POST);
 ?>
